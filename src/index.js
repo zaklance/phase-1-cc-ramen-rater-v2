fb098d8b78c79e77cb68da8eeb3a380cfd8afc72
zaklance <wosewick@gmail.com>
2024-05-01 16:41:36 -0400

    add top bar of images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 // index.js
+fetch('http://localhost:3000/ramens')
+.then((res) => res.json())
+.then(ramenMenu => {
+  const ramenDisplay = document.querySelector('#ramen-menu');
+  ramenMenu.forEach(ramen => {
+    const img = document.createElement('img');
+    img.src = `${ramen.image}`;
+    ramenDisplay.append(img);
+  });
+});
 
 // Callbacks
 const handleClick = (ramen) => {
